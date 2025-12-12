9de10b904f23ee78ca2de3ff401b9625efa85f45
Frendy22 <greenson22022002@gmail.com>
2025-12-12 22:10:26 +0800

    delete: menghapus file yang tidak perlu

--- lib/core/services/gemini_storage_service.dart
+++
@@ -1,7 +0,0 @@
-// lib/core/services/gemini_storage_service.dart
-//
-// SEMUA FUNGSI DARI FILE INI TELAH DIPINDAHKAN KE
-// lib/features/settings/application/gemini_settings_service.dart
-// dan dikelola melalui file JSON terpusat.
-//
-// File ini dapat dihapus dengan aman dari proyek.
